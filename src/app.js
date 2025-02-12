fd16a5169c7fa82b03b6c48001c42fdad690bb70
piyushk1302 <piyushk1302@gmail.com>
2025-02-12 13:21:38 +0530

    route handlers

--- src/app.js
+++ src/app.js
@@ -9,6 +9,23 @@ const app = express();
 //   });
 // });
 
+app.use("/user",[(req,res,next)=>{
+  console.log("Handling the router 1");
+  // res.send("Router 1 handled successfully");
+  next();
+  res.send("Router 1 handled successfully");
+},(req,res,next)=>{
+  console.log("hello");
+  
+  //res.send("Router 2 handled success")
+  next()
+},(req,res,next)=>{
+  console.log("hello 3");
+  
+  res.send("Router 3 handled success")
+  next()
+}])
+
 //ab*d-anything in place of *
 
 app.get("/ab+c", (req, res) => {
@@ -26,41 +43,41 @@ app.get("/ab+c", (req, res) => {
 //     });
 //   });
 
-app.get("/user/:userId", (req, res) => {
-    console.log(req.params);
-    res.send({
-      fname: "Piyush",
-      lname: "Singh",
-    });
-  });
+// app.get("/user/:userId", (req, res) => {
+//     console.log(req.params);
+//     res.send({
+//       fname: "Piyush",
+//       lname: "Singh",
+//     });
+//   });
 
-app.post("/user", (req, res) => {
-  // saving data to the database
-  res.send("data saved to db successfully");
-});
+// app.post("/user", (req, res) => {
+//   // saving data to the database
+//   res.send("data saved to db successfully");
+// });
 
-app.put("/user", (req, res) => {
-  // update data in the database
-  res.send("data updated to db successfully");
-});
+// app.put("/user", (req, res) => {
+//   // update data in the database
+//   res.send("data updated to db successfully");
+// });
 
-app.patch("/user", (req, res) => {
-  // update data in the database
-  res.send("data patched to db successfully");
-});
+// app.patch("/user", (req, res) => {
+//   // update data in the database
+//   res.send("data patched to db successfully");
+// });
 
-app.delete("/user", (req, res) => {
-  // deleting data from the database
-  res.send("data deleted from db successfully");
-});
+// app.delete("/user", (req, res) => {
+//   // deleting data from the database
+//   res.send("data deleted from db successfully");
+// });
 
-app.get("/test", (req, res) => {
-  res.send("Hello from server(test) side");
-});
+// app.get("/test", (req, res) => {
+//   res.send("Hello from server(test) side");
+// });
 
-app.get("/hello", (req, res) => {
-  res.send("Hello hello hello hello");
-});
+// app.get("/hello", (req, res) => {
+//   res.send("Hello hello hello hello");
+// });
 
 app.listen(7777, () => {
   console.log("Server created and is listening on port 7777...");
